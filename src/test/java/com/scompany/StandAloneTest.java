a04344b6158c14d354464a1242eae579874a4939
Sajitha <ssajitha7@gmail.com>
2024-08-16 00:52:31 +0400

    end 2 end project setup completed code refactoring yet to start

--- src/test/java/com/scompany/StandAloneTest.java
+++ src/test/java/com/scompany/StandAloneTest.java
@@ -3,17 +3,21 @@ package com.scompany;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 public class StandAloneTest {
 
@@ -22,6 +26,7 @@ public class StandAloneTest {
 		String itemName= "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://rahulshettyacademy.com/client");
 		driver.findElement(By.id("userEmail")).sendKeys("saj@gmail.com");
@@ -44,9 +49,24 @@ public class StandAloneTest {
 		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("#toast-container"))));
 		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
 		
-		List<WebElement> cartItems= driver.findElements(By.cssSelector("cartSection.h3"));
-		cartItems.stream().anyMatch(cartItem->cartItem.getText().equals(itemName));
-		//btn.btn-primary
+		List<WebElement> cartItems= driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match = cartItems.stream().anyMatch(cartItem->cartItem.getText().equals(itemName));
+		Assert.assertTrue(match);
+		
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Select Country']")).sendKeys("uni");
+		
+		//alternate method to type uni
+		//Actions act =new Actions(driver);
+		//act.sendKeys(driver.findElement(By.xpath("//input[@placeholder='Select Country']")),"uni").build().perform();
+		
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//section[contains(@class,'ta-results')]"))));
+		driver.findElement(By.xpath("//button[contains(@class,'ta-item')][4]")).click();
+		driver.findElement(By.xpath("//a[contains(@class,'btnn')]")).click();
+		
+		String confirmMessage= driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("Thankyou for the order."));
+		driver.close();
 	}
 
 }
